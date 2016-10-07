63885e1600ff5c992d1e08bb3bd2e561de84db3d
Andrushchenko, Oleksandr <andr2000@gmail.com>
2016-10-07 14:20:30 +0300

    Cleanup and code-style changes
    Signed-off-by: Andrushchenko, Oleksandr <andr2000@gmail.com>

--- pv_audiofront.c
+++ pv_audiofront.c
@@ -13,21 +13,16 @@
 #ifdef SILENT
 #define LOG(log_level, fmt, ...)
 #else
-#define LOG(log_level, fmt, ...) 								\
-		do {													\
-			printk(AUDIO_DEVICE_NAME #log_level " (%s:%d): " fmt "\n",	\
-					__FUNCTION__, __LINE__ , ## __VA_ARGS__);	\
+#define LOG(log_level, fmt, ...) \
+		do { \
+			printk(AUDIO_DEVICE_NAME #log_level " (%s:%d): " fmt "\n", \
+					__FUNCTION__, __LINE__ , ## __VA_ARGS__); \
 		} while (0)
 #endif
 
 #define LOG0(fmt, ...) do if (debug_level >= 0) LOG(0, fmt, ## __VA_ARGS__); while (0)
 
 int debug_level;
-
-static const struct xenbus_device_id xenaudio_ids[] = {
-	{ AUDIO_DEVICE_NAME },
-	{ "" }
-};
 
 static int xenaudio_probe(struct xenbus_device *dev,
 				  const struct xenbus_device_id *id)
@@ -54,6 +49,11 @@ static int xenaudio_resume(struct xenbus_device *dev)
 	return 0;
 }
 
+static const struct xenbus_device_id xenaudio_ids[] = {
+	{ AUDIO_DEVICE_NAME },
+	{ "" }
+};
+
 static struct xenbus_driver xenaudio_driver = {
 	.ids = xenaudio_ids,
 	.probe = xenaudio_probe,
@@ -64,13 +64,14 @@ static struct xenbus_driver xenaudio_driver = {
 
 static int __init xenaudio_init(void)
 {
-	if (!xen_domain()) {
+	if (!xen_domain())
 		return -ENODEV;
-	}
 	if (xen_initial_domain()) {
 		LOG0(AUDIO_DEVICE_NAME " cannot run in Dom0");
 		return -ENODEV;
 	}
+	if (!xen_has_pv_devices())
+		return -ENODEV;
 	LOG0("Registering XEN PV " AUDIO_DEVICE_NAME);
 	return xenbus_register_frontend(&xenaudio_driver);
 }
